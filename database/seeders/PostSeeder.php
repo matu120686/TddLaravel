1b49baea0502e16fb51b1f2625675a1e26d59cf4
Matias Olivera <matu120686@gmail.com>
2022-09-01 20:54:55 -0300

    creo migracion, modelo, seeder para Post

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Database\Factories\PostFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory(50)->create();
+    }
+}
